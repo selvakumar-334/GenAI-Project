9298f196fb7f00959ba7b7cbf58bfad3e1b94c8f
selvakumar-334 <selvakumarsingaram20@gmail.com>
2026-04-15 23:43:39 +0530

    I have completed the variables task

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-print ("Thanks for your github session shiva")
+product_name= input("Enter a product name")
+price = int(input("Enter the price"))
+quantity=int(input("Enter a quantity"))
+total_cost= (price*quantity)
+print("product name", product_name)
+print("price is", str(price))
+print("quanitiy is", str(quantity))
+print("Total cost is", total_cost)
+
+print(type(total_cost))
---
+++ variables.py
@@ -0,0 +1,64 @@
+name = "Selva"
+age = 39
+Salary = 1000000
+print("name" , name)
+print ("Age is", age)
+print ("Salary is", Salary)
+
+x=20
+y=30
+z=40
+print(x,y,z)
+
+a=b=c=1000
+print (a,b,c)
+name, age, city= "Kani", 37, "Chennai"
+print(name, age, city)
+
+x=50
+print (x)
+x="Hello Selva"
+print (x)
+
+age = 25
+price = 99.5
+name = "Selva"
+is_active = True
+print(age)
+print(price)
+print(name)
+print(is_active)
+
+x=10
+y="Selva"
+z=10.5
+print(type(x))
+print(type(y))
+print(type(z))
+
+name= input("Enter the student name")
+age= input("Enter the student age")
+course= input("Enter the course name")
+print("Student name", name)
+print("Student age", age)
+print("Course name", course)
+
+10
+student_name= input("Enter a Name")
+age = input("Enter the age")
+city = input("Enter the City")
+
+print("name:", student_name)
+print("Age is", str(age))
+print("City is, city)
+
+product_name= input("Enter a product name")
+price = int(input("Enter the price"))
+quantity=int(input("Enter a quantity"))
+total_cost= (price*quantity)
+print("product name", product_name)
+print("price is", str(price))
+print("quanitiy is", str(quantity))
+print("Total cost is", total_cost)
+
+print(type(total_cost))
